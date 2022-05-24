d19718bbbd673343c8d733abcb7cbe4ac3cfb79f
duyphuong <you@example.com>
2022-05-24 20:53:21 +0700

    refactor: remove commented code

--- gke.tf
+++ gke.tf
@@ -63,18 +63,3 @@ resource "google_container_node_pool" "primary_nodes" {
     }
   }
 }
-
-# provider "kubernetes" {
-#   load_config_file = false
-#   host                   = "https://${google_container_cluster.primary.endpoint}"
-#   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-#   token                  = data.google_client_config.current.access_token
-# }
-
-# provider "kubectl" {
-#   load_config_file       = false
-#   host                   = "https://${google_container_cluster.primary.endpoint}"
-#   token                  = "${google_container_cluster.primary.access_token}"
-#   cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
-# }
-
--- services.tf
+++ services.tf
@@ -1,8 +1,6 @@
 provider "helm" {
   kubernetes {
-    # config_path = "~/.kube/config"
   host                   = "https://${google_container_cluster.primary.endpoint}"
-#   token                  = "${google_container_cluster.primary.access_token}"
   token                  = "${data.google_client_config.default.access_token}"
   cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
   }
