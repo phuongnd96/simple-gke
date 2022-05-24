9084371f9d6b16c3829c6dfb5a7d50da7e329166
duyphuong <you@example.com>
2022-05-24 08:25:01 +0700

    update local vars

--- gke.tf
+++ gke.tf
@@ -13,7 +13,7 @@ resource "random_shuffle" "available_zones" {
 locals {
   location = var.regional ? var.region : var.zones[0]
   node_locations = var.regional ? coalescelist(compact(var.zones), sort(random_shuffle.available_zones.result)) : slice(var.zones, 1, length(var.zones))
-  # node_locations = slice(var.zones, 1, length(var.zones))
+  region   = var.regional ? var.region : join("-", slice(split("-", var.zones[0]), 0, 2))
 }
 
 # GKE cluster
