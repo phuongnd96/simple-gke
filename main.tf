80478a76e0b56ec3c54cafc47950366e1df42089
duyphuong <you@example.com>
2022-05-24 22:24:52 +0700

    fix: change argument to block

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "google_container_cluster" "primary" {
   initial_node_count        = var.initial_node_count
   network                   = var.network
   subnetwork                = var.subnetwork
-  ip_allocation_policy      = {
+  ip_allocation_policy      {
     cluster_secondary_range_name  = var.pods_secondary_ip_range_name
     services_secondary_range_name = var.services_secondary_ip_range_name
   }
